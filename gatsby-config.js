1f6748779e06ad9f9422be19db38065023b39586
Garm <garm@Garms-MacBook-Pro.local>
2018-06-05 11:38:04 +0200

    added gatby md plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,13 @@ module.exports = {
     plugins: [
         "gatsby-plugin-typescript",
         "gatsby-plugin-netlify-cms",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                path: "@{__dirname}/blog",
+                name: "markdown-pages"
+            }
+        },
+        "gatsby-trandformer-remark",
     ]
 }
