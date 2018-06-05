c64febf54575ebceed1ee2d2fd6ab8a395effa78
Garm <garm@Garms-MacBook-Pro.local>
2018-06-06 01:42:52 +0200

    changed base layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,8 +1,20 @@
 import React from "react"
+import { Helmet } from "react-helmet" 
+import styled from "styled-components"
+
+const Container = styled.div`
+    margin: 3rem auto;
+    max-width: 600px;
+`
 
 export default ({ children }) => (
     <div>
-        <h1>Studentforeningen</h1>
-        {children()}
+        <Helmet>
+            <title>Studentföreningen</title>
+        </Helmet>
+        <h1>Studentföreningen</h1>
+        <Container>
+            {children()}
+        </Container>
     </div>
 )
