923e72d758ebde6aef3285edd41abbd225aad914
Garm <garm@Garms-MacBook-Pro.local>
2018-06-07 02:06:32 +0200

    styled components bugfix

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     },
     plugins: [
         "gatsby-plugin-typography",
+        "gatsby-plugin-styled-components",
         "gatsby-plugin-react-helmet",
         "gatsby-plugin-netlify-cms",
         {
